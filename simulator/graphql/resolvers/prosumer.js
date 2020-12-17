3d11f347c648cd7d5c80256c92eb06eb70545951
Christoffer Fink <chrfin-4@student.ltu.se>
2020-12-17 01:45:59 +0100

    Unbroke simulation resolvers

--- simulator/graphql/resolvers/prosumer.js
+++ simulator/graphql/resolvers/prosumer.js
@@ -2,17 +2,18 @@ const model = require('../../model');
 
 const sim = model.simulation;
 
+// Note: Args are now in parent rather than args.
 module.exports = {
   prosumerStates: () => sim.prosumerStates(),
-  prosumerState: (_, args) => sim.prosumerState(args.id),
-  setChargeRatio: (_, { id, ratio }) => {
+  prosumerState: ({ id }) => sim.prosumerState(parent.id),
+  setChargeRatio: ({ id, ratio }) => {
     try {
       return sim.prosumer(id).setChargeRatio(ratio).currentState();
     } catch (e) {
       return sim.prosumer(id).currentState();  // Return unchanged.
     }
   },
-  setDischargeRatio: (_, { id, ratio }) => {
+  setDischargeRatio: ({ id, ratio }) => {
     try {
       return sim.prosumer(id).setDischargeRatio(ratio).currentState();
     } catch (e) {
