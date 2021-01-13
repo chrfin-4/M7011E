778f94b808d871aca0d88b0a0dba4245dd05f39b
Emily Åkerström <emily.akerstrom@gmail.com>
2021-01-13 21:27:56 +0100

    Changed from isauth middleware to express-session with redis keyvalue store

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const { __prod__, COOKIE_NAME } = require('./constants');
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { graphqlHTTP } = require('express-graphql');
@@ -8,20 +9,55 @@ const { fetch } = require('cross-fetch');
 const { print } = require('graphql');
 const mongoose = require('mongoose');
 const graphQlResolvers = require('./graphql/resolvers/index');
-const isAuth = require('./middleware/is-auth');
+const { RedisClient} = require('redis');
+const session = require('express-session');
+const connectRedis = require('connect-redis');
 
-const app = express();
-
-const port = 8080;
 
 const main = async () => {
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redis = new RedisClient(process.env.REDIS_URL);
+  app.set("trust proxy", 1);
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+        domain: __prod__ ? ".codeponder.com" : undefined,
+      },
+      saveUninitialized: false,
+      secret: process.env.CRYPT_KEY,
+      resave: false,
+    })
+  );
+
+  app.use(bodyParser.json());
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
+  });
+  
   // Load schema from the file
   const graphQlSchema = loadSchemaSync(join(__dirname, './graphql/schema.graphql'), {
     loaders: [
       new GraphQLFileLoader(),
     ]
   });
-
 
   let localSchema = addResolversToSchema({
     schema: graphQlSchema,
@@ -55,37 +91,18 @@ const main = async () => {
     ]
   });
 
-  app.use(bodyParser.json());
-
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200);
-    }
-    next();
-  });
-  
-  app.use(isAuth);
-
-  const apolloServer = await new ApolloServer({
-    schema: gatewaySchema
+  const apolloServer = new ApolloServer({
+    schema: gatewaySchema,
+    context: ({ req, res}) => ({
+      req,
+      res,
+      redis
+    }),
   });
 
-  apolloServer.applyMiddleware({ app });
-
-  /*
-  app.use(
-    '/graphql',
-    graphqlHTTP({
-      schema: gatewaySchema,
-      graphiql: {
-        headerEditorEnabled: true
-      }
-    })
-  );
-  */
+  apolloServer.applyMiddleware({
+    app,
+  });
 
   // ==== Start server ====
 
@@ -93,7 +110,7 @@ const main = async () => {
     `${process.env.MONGO_CONNECTION}`
   )
     .then(() => {
-      app.listen(port);
+      app.listen(parseInt(process.env.PORT));
     })
     .catch(err => {
       console.log(err);
---
+++ server/src/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  __prod__: process.env.NODE_ENV === "production",
+  COOKIE_NAME: "qid",
+  FORGET_PASSWORD_PREFIX: "forget-password:"
+}; 
