c521226c09e34c85209cdc39318e3bf4f96d8807
Emily Åkerström <emily.akerstrom@gmail.com>
2021-01-18 03:08:01 +0100

    /stats done

--- web/pages/stats.js
+++ web/pages/stats.js
@@ -1,11 +1,16 @@
-import { Container } from 'next/app';
 import React from 'react'
+import PropTypes from 'prop-types';
 import { withApollo } from "../src/utils/withApollo";
+import { useApolloClient } from '@apollo/client';
+import { isServer } from '../src/utils/isServer';
 
 import { 
   useMeQuery, 
   useOwnedQuery,
-  useSimDataQuery,
+  useProsumerDataQuery,
+  useMarketDataQuery,
+  useSetChargeRatioMutation,
+  useSetDischargeRatioMutation,
 } from '../src/generated/graphql.ts'
 
 import clsx from 'clsx';
@@ -20,25 +25,119 @@ import {
   Card,
   CardContent,
   CardActions,
+  Paper,
+  TableBody,
+  TableContainer,
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  Collapse,
+  IconButton,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { LoadingButton} from '@material-ui/lab';
-import { useApolloClient } from '@apollo/client';
-import { isServer } from '../src/utils/isServer';
+import { TextField } from 'formik-material-ui';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import { Field, Form, Formik } from 'formik';
 
 const useStyles = makeStyles((theme) => ({
-
+  manage: {
+    margin: 'auto'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+  },
+  actionPaper: {
+    maxWidth: 230
+  },
+  table: {
+    // minWidth: 650,
+  },
+  hideLastBorder: {
+    '& > *': {
+      borderBottom: 'unset',
+    },
+  },
 }));
+
+function BatteryRow(props) {
+  const { charge, capacity } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useStyles();
+
+  return (
+    <>
+      <TableRow className={clsx(classes.hideLastBorder)}>
+        <TableCell component="th" scope="row">
+          Battery level <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell aria-label="expand row" align="right">
+          {(charge / capacity * 100).toFixed(1) + '%'}
+        </TableCell>
+      </TableRow>
+      <TableRow className={clsx(classes.hideLastBorder)}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout='auto' unmountOnExit>
+            <Table size="small" aria-label="battery">
+              <TableHead>
+                <TableRow className={clsx(classes.hideLastBorder)}>
+                  <TableCell>Stat</TableCell>
+                  <TableCell align="right">Data</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow className={clsx(classes.hideLastBorder)}>
+                  <TableCell>Charge</TableCell>
+                  <TableCell align="right">
+                    {charge.toFixed(3)}
+                  </TableCell>
+                </TableRow>
+                <TableRow className={clsx(classes.hideLastBorder)}>
+                  <TableCell>Capacity</TableCell>
+                  <TableCell align="right">
+                    {capacity.toFixed(3)}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  )
+}
+
+BatteryRow.propTypes = {
+  charge: PropTypes.number.isRequired,
+  capacity: PropTypes.number.isRequired,
+};
 
 const Stats = ({}) => {
   const classes = useStyles();
   const apolloClient = useApolloClient();
+  const [setChargeRatio] = useSetChargeRatioMutation();
+  const [setDischargeRatio] = useSetDischargeRatioMutation();
+
   const { data: meData, loading: meLoading } = useMeQuery({
     skip: isServer(),
   });
 
-  const { data: simData, loading: simLoading } = useSimDataQuery({
+  const { data: psData, loading: psLoading } = useProsumerDataQuery({
     variables: { id: meData?.me.prosumerData.houseId },
+    skip: meData === undefined,
+    pollInterval: 1000,
+  });
+
+  const { data: mdData, loading: mdLoading } = useMarketDataQuery({
     skip: meData === undefined,
     pollInterval: 1000,
   });
@@ -48,38 +147,235 @@ const Stats = ({}) => {
   let marketBody = null;
   let manageBody = null;
 
-  if (meLoading || simLoading) {
+  if (meLoading || psLoading || mdLoading) {
   } else if (meData?.me) {
-
-    let hasHouse = meData.me.prosumerData.houseId !== undefined && meData.me.prosumerData.houseId !== null;
+    let hasHouse = meData.me?.prosumerData.houseId !== undefined && meData.me?.prosumerData.houseId !== null;
+    let pState = psData?.prosumerState;
+    let weather = mdData?.weather;
     houseBody = (
-      <Box>
-        HouseBody {simData.marketDemand}
-      </Box>
+      <TableContainer component={Paper} className={clsx(classes.paper)}>
+        <Typography variant="h6">
+          House
+        </Typography>
+        <Table className={clsx(classes.table)} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Stat</TableCell>
+              <TableCell align="right">Data</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Power consumption</TableCell>
+              <TableCell align="right">{hasHouse ? pState.powerConsumption.toFixed(3) : "No house"}</TableCell>
+            </TableRow>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Power production</TableCell>
+              <TableCell align="right">{hasHouse ? pState.powerProduction.toFixed(3) : "No house"}</TableCell>
+            </TableRow>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Charge ratio</TableCell>
+              <TableCell align="right">{hasHouse ? pState.chargeRatio.toFixed(3) : "No house"}</TableCell>
+            </TableRow>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Discharge ratio</TableCell>
+              <TableCell align="right">{hasHouse ? pState.dischargeRatio.toFixed(3) : "No house"}</TableCell>
+            </TableRow>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Production status [remove me?]</TableCell>
+              <TableCell align="right">{hasHouse ? pState.productionStatus.toFixed(3) : "No house"}</TableCell>
+            </TableRow>
+            {hasHouse ? (
+              <BatteryRow charge={pState.battery.charge} capacity={pState.battery.capacity}/>
+            ) : (
+              <TableRow className={clsx(classes.hideLastBorder)}>
+                <TableCell>Battery level</TableCell>
+                <TableCell align="right">No house</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
     weatherBody = (
-      <Box>
-        WeatherBody
-      </Box>
+      <TableContainer component={Paper} className={clsx(classes.paper)}>
+        <Typography variant="h6">
+          Weather
+        </Typography>
+        <Table className={clsx(classes.table)} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Stat</TableCell>
+              <TableCell align="right">Data</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Windspeed</TableCell>
+              <TableCell align="right">{weather.windSpeed.toFixed(3)}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
     marketBody = (
-      <Box>
-        MarketBody
-      </Box>
+      <TableContainer component={Paper} className={clsx(classes.paper)}>
+        <Typography variant="h6">
+          Market
+        </Typography>
+        <Table className={clsx(classes.table)} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Stat</TableCell>
+              <TableCell align="right">Data</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Market demand</TableCell>
+              <TableCell align="right">{mdData.marketDemand.toFixed(0).replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</TableCell>
+            </TableRow>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Current price</TableCell>
+              <TableCell align="right">{mdData.currentPrice.toFixed(3)}</TableCell>
+            </TableRow>
+            <TableRow className={clsx(classes.hideLastBorder)}>
+              <TableCell component="th" scope="row">Modeled price</TableCell>
+              <TableCell align="right">{mdData.modeledPrice.toFixed(3)}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
     manageBody = (
-      <Box>
-        ManageBody
-      </Box>
+      <Paper className={clsx(classes.paper)}>
+        <Typography variant="h6">
+          Manage
+        </Typography>
+        <Grid container spacing={2} justifyContent="flex-start">
+          <Grid item xs={12} md={6}>
+            <Paper className={clsx(classes.paper, classes.actionPaper)}>
+              <Typography variant="h6" gutterBottom>
+                Set charge ratio
+              </Typography>
+              <Formik
+                initialValues={{ ratio: 0 }}
+                onSubmit={async (values) => {
+                  console.log(values);
+                  const response = await setChargeRatio({
+                    variables: {
+                      id: meData.me.prosumerData.houseId,
+                      ratio: values.ratio
+                    }
+                  });
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    <Grid container spacing={1} alignItems="center">
+                      <Grid item xs={12} sm={6}>
+                        <Field
+                          component={TextField}
+                          name="ratio"
+                          type="number"
+                          label="Ratio"
+                          defaultValue={0.4}
+                          inputProps={{ min: 0, max: 1, step: 0.001 }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <LoadingButton
+                          size="small"
+                          type="submit"
+                          variant="contained"
+                          pending={isSubmitting}
+                          sx={{ height: 50 }}
+                        >
+                          Submit
+                        </LoadingButton>
+                      </Grid>
+                    </Grid>
+                  </Form>
+                )}
+              </Formik>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Paper className={clsx(classes.paper, classes.actionPaper)}>
+              <Typography variant="h6" gutterBottom>
+                Set discharge ratio
+              </Typography>
+              <Formik
+                initialValues={{ ratio: 0 }}
+                onSubmit={async (values) => {
+                  console.log(values);
+                  const response = await setDischargeRatio({
+                    variables: {
+                      id: meData.me.prosumerData.houseId,
+                      ratio: values.ratio
+                    }
+                  });
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    <Grid container spacing={1} alignItems="center">
+                      <Grid item xs={12} sm={6}>
+                        <Field
+                          component={TextField}
+                          name="ratio"
+                          type="number"
+                          label="Ratio"
+                          defaultValue={0.4}
+                          inputProps={{ min: 0, max: 1, step: 0.001 }}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={6}>
+                        <LoadingButton
+                          size="small"
+                          type="submit"
+                          variant="contained"
+                          pending={isSubmitting}
+                          sx={{ height: 50 }}
+                        >
+                          Submit
+                        </LoadingButton>
+                      </Grid>
+                    </Grid>
+                  </Form>
+                )}
+              </Formik>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Paper>
     )
   }
 
   return (
-    <Box>
-      <Box>{houseBody}</Box>
-      <Box>{weatherBody}</Box>
-      <Box>{marketBody}</Box>
-      <Box>{manageBody}</Box>
+    <Box sx={{ overflow: "hidden", mx: 2 }}>
+      <Grid container spacing={2} justifyContent="center" >
+        <Grid item xs={12} md={8}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>{houseBody}</Grid>
+            <Grid item xs={12} md={6}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Box>{weatherBody}</Box>
+                </Grid>
+                <Grid item xs={12}>
+                  <Box>{marketBody}</Box>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Box sx={{ display: "grid", gap: 8, gridAutoFlow: "row" }}>
+            <Box>{manageBody}</Box>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   )
 };
